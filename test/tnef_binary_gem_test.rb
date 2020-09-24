f75195821624a42f825227b478484568364e6fda
Arandi Lopez <arandilopez.93@gmail.com>
2020-09-24 15:53:37 -0500

    Add winmail.dat to test against the binaries

--- test/tnef_binary_gem_test.rb
+++ test/tnef_binary_gem_test.rb
@@ -7,4 +7,17 @@ class TnefBinaryGemTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::TnefBinaryGem::VERSION
   end
+
+  def test_execute_binary
+    output = `bundle exec tnef --help`
+    refute_nil output
+  end
+
+  def test_list_files_in_winmail_dat
+    output = `bundle exec tnef --list test/fixtures/files/winmail.dat`
+    files = output.lines.map { |line| line.split('|').first.strip }
+    assert_equal 2, files.size
+    assert files.include?('zappa_av1.jpg')
+    assert files.include?('bookmark.htm')
+  end
 end
